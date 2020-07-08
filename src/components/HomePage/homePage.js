59ea77a11f8750c8fd45022bf8814c36349473f2
Nicholas Jaquez <nrjaquez@gmail.com>
2020-07-07 23:07:08 -0700

    paragraph slimmed

--- src/components/HomePage/homePage.js
+++ src/components/HomePage/homePage.js
@@ -9,12 +9,13 @@ function homePage() {
       <h3>Full-Stack Web Developer</h3>
       <h3>HTML/CSS || Node JS || Express || React || MongoDB || MySQL</h3>
     
-    
+        <div className="content">
         <p className="aboutMe">
             My name is Nicholas Jaquez and I am a Full-Stack Web Developer. I have received my certificate in Coding from
             the UCLA Extension Program. I can work with MongoDB, Express, React, and Node JS. I am also willing to 
             learn more software languages. Please feel free to look at my projects here and contact me.
         </p>
+        </div>
         </div>
   );
 }
