8e3159ee46c2305120e77a91fdd365da3936bcc4
Nicholas Jaquez <nrjaquez@gmail.com>
2020-07-16 18:34:18 -0700

    WeTrivia image appearing

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../Projects/projects"
+import WeTriva from "../../images/WeTrivia.png"
 
 function Resume(){
     return (
@@ -7,13 +8,9 @@ function Resume(){
             <h1>
                 Projects
             </h1>
-            <div>
-                <p><a href="https://intense-earth-41726.herokuapp.com/">We Trivia</a></p>
-                <p><a href="https://github.com/mchisem/Weather-or-Not">Weather or Not</a></p>
-                <p><a href="https://stark-sands-40022.herokuapp.com/">The Progressive Budget App</a></p>
-                <p><a href="https://stark-forest-71498.herokuapp.com/?id=5eade51e1a393f0017ed331b">The Fitness Tracker App</a></p>
-                <p><a href="https://frozen-cove-72055.herokuapp.com/burgers">The Burger App</a></p>
-                <p><a href="https://polar-spire-92196.herokuapp.com/">Employee Directory</a></p>
+            <div className="weTrivia">
+                <p><img src={WeTriva}/><a href="https://intense-earth-41726.herokuapp.com/">We Trivia</a><br></br>
+                <a href="https://github.com/nickrayj45/WeTrivia">GitHub</a></p>
             </div>
         </div>
     )
